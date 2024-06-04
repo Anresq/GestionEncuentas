2ce75ff97a8bd5cd6d83d7fb29e36b46d546995d
anresq <restrepoana11032013@gmail.com>
2024-06-04 06:42:48 -0500

    configuracion inicial del proyecto

---
+++ GestionEncuestas/src/test/java/A/R/GestionEncuestas/GestionEncuestasApplicationTests.java
@@ -0,0 +1,13 @@
+package A.R.GestionEncuestas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEncuestasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
